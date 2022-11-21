4b5165304d7f8a1468997a97e3a490cc4b2e1e11
Joaquin Sierra <joaquinsierra1679@gmail.com>
2022-11-21 16:39:29 -0300

    Entrega N3 javascript

--- js/script.js
+++ js/script.js
@@ -1,78 +1,159 @@
-class Producto {
+const usuarios = [{
+    nombre: 'Lorena',
+    mail: 'lorena1980@hotmail.com',
+    pass: 'coticoti'
+},
+{
+    nombre: 'Fatima',
+    mail: 'fati15@gmail.com',
+    pass: 'holahola'
+},
+{
+    nombre: 'Bartolome',
+    mail: 'bartolomeimba@gmail.com',
+    pass: 'bocaboca'
+}]
 
-    constructor(nombre, categoria, precio, id) {
-        this.nombre = nombre;
-        this.categoria = categoria;
-        this.precio = parseInt(precio);
-        this.id = id;
+const productos = [{
+    nombre: "Antifaz Gris",
+    categoria: "disfraz",
+    precio: 300,
+    img: './imgs/antifazgris.jpg'
+}, {
+    nombre: "Banderin Feliz cumpleaños",
+    categoria: "decoracion",
+    precio: 700,
+    img: './imgs/banderinfc.jpg'
+}, {
+    nombre: "Cañon de papelitos",
+    categoria: "fiesta",
+    precio: 900,
+    img: './imgs/lanzapapelitos.jpg'
+}, {
+    nombre: "Velas",
+    categoria: "decoracion",
+    precio: 50,
+    img: './imgs/velasbicolor.jpg'
+}]
+
+//Como con la profe pongo los elementos del DOM que necesito//
+
+const inputMailLogin = document.getElementById('emailLogin'),
+    inputPassLogin = document.getElementById('passwordLogin'),
+    checkRecordar = document.getElementById('recordarme'),
+    btnLogin = document.getElementById('login'),
+    modalEl = document.getElementById('modalLogin'),
+    modal = new bootstrap.Modal(modalEl),
+    contTarjetas = document.getElementById('tarjetas'),
+    elementosToggleables = document.querySelectorAll('.toggeable');
+
+
+
+    // La funcion para validar el usuario//
+function validarUsuario(usersDB, user, pass) {
+    let encontrado = usersDB.find((userDB) => userDB.mail == user);
+    console.log(encontrado)
+    console.log(typeof encontrado)
+
+    if (typeof encontrado === 'undefined') {
+        return false;
+    } else {
+        if (encontrado.pass != pass) {
+            return false;
+        } else {
+            return encontrado;
+        }
     }
-
-    asignarId(array) {
-        this.id = array.length;
-    }
-
 }
 
-const productos = [
-    new Producto('Antifaz Blanco', 'Fiesta', 600, 1),
-    new Producto('Coco Rallado', 'Alimento', 120, 2),
-    new Producto('Mantel', 'Decoracion', 1200, 3),
-    new Producto('Mascara Jason', 'Fiesta', 300, 4),
-    new Producto('Sangre Falsa', 'Fiesta', 400, 5),
-    new Producto('Globo Comun', 'Fiesta', 30, 6),
-]
-
-let continuar = true;
-
-while (continuar) {
-
-    let ingreso = prompt('Ingresa los datos del producto: nombre, categoria, precio, separados por una barra diagonal (/). Ingresa X para finalizar');
-
-    if (ingreso.toUpperCase() == 'X') {
-        continuar = false;
-        break;
+function guardarDatos(usuarioDB, storage) {
+    const usuario = {
+        'name': usuarioDB.nombre,
+        'user': usuarioDB.mail,
+        'pass': usuarioDB.pass,
     }
 
-    const datos = ingreso.split('/');
-    console.log(datos);
-
-    const producto = new Producto(datos[0], datos[1], datos[2]);
-
-    productos.push(producto);
-    producto.asignarId(productos);
-    console.log(productos);
-
-}
-let criterio = prompt('Elegí el criterio deseado:\n1 - Nombre (A a Z) \n2 - Nombre (Z a A)\n3 - Mayor a Menor Precio');
-
-function ordenar(criterio, array) {
-    let arrayOrdenado = array.slice(0);
-
-    switch (criterio) {
-        case '1':
-            let nombreAscendente = arrayOrdenado.sort((a, b) => a.nombre.localeCompare(b.nombre));
-            return nombreAscendente;
-        case '2':
-            return arrayOrdenado.sort((a, b) => b.nombre.localeCompare(a.nombre));
-
-        case '3':
-            return arrayOrdenado.sort((a, b) => b.precio - a.precio);
-        default:
-            alert('No es un criterio válido, te mostraremos el catálogo sin ordenar');
-            return arrayOrdenado;
-    }
-
+    storage.setItem('usuario', JSON.stringify(usuario));
 }
 
-function crearStringResultado(array) {
-    let info = '';
+function borrarDatos() {
+    localStorage.clear();
+    sessionStorage.clear();
+}
 
-    array.forEach(elemento => {
-        info += 'Nombre: ' + elemento.nombre + '\nCategoria: ' + elemento.categoria + '\nPrecio: ' + elemento.precio + '\n\n'
+function recuperarUsuario(storage) {
+    return JSON.parse(storage.getItem('usuario'));
+}
+
+function saludar(usuario) {
+    nombreUsuario.innerHTML = `Bienvenido/a, <span>${usuario.name}</span>`
+}
+
+
+function mostrarInfoProductos(array) {
+    contTarjetas.innerHTML = '';
+    array.forEach(element => {
+        let html = `<div class="card cardProducto" id="tarjeta${element.nombre}">
+                <h3 class="card-header" id="nombreProducto">Nombre: ${element.nombre}</h3>
+                <img src="${element.img}" alt="${element.nombre}" class="card-img-bottom" id="fotoProducto">
+                <div class="card-body">
+                    <p class="card-text" id="categoriaProducto">Categoria: ${element.categoria}</p>
+                    <p class="card-text" id="precioProducto">Precio: ${element.precio} pesos</p>
+                </div>
+            </div>`;
+        contTarjetas.innerHTML += html;
     });
-
-    return info;
-
 }
 
-alert(crearStringResultado(ordenar(criterio, productos)));
+
+function presentarInfo(array, clase) {
+    array.forEach(element => {
+        element.classList.toggle(clase);
+    });
+}
+
+function estaLogueado(usuario) {
+
+    if (usuario) {
+        saludar(usuario);
+        mostrarInfoProducto(productos);
+        presentarInfo(elementosToggleables, 'd-none');
+    }
+}
+
+
+btnLogin.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    //En este paso verificamos que los campos a rellenar sean completados//
+    if (!inputMailLogin.value || !inputPassLogin.value) {
+        alert('Todos los campos tienen que estar completos');
+    } else {
+        let data = validarUsuario(usuarios, inputMailLogin.value, inputPassLogin.value);
+
+        if (!data) {
+            alert(`Usuario y/o contraseña erróneos`);
+        } else {
+            //este paso es por si quiere que se recuerde su informacion//
+            if (checkRecordar.checked) {
+                guardarDatos(data, localStorage);
+                saludar(recuperarUsuario(localStorage));
+            } else {
+                guardarDatos(data, sessionStorage);
+                saludar(recuperarUsuario(sessionStorage));
+            }
+            
+            modal.hide();
+            
+            mostrarInfoProductos(productos);
+            presentarInfo(elementosToggleables, 'd-none');
+        }
+    }
+});
+
+btnLogout.addEventListener('click', () => {
+    borrarDatos();
+    presentarInfo(elementosToggleables, 'd-none');
+});
+
+estaLogueado(recuperarUsuario(localStorage));
